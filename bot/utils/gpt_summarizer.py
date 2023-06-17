7ab1dadcd0d9fc437f719844e14958aa5aa415ff
advait <advait001@e.ntu.edu.sg>
2023-06-17 20:35:15 +0800

    Switch to env var for OpenAI

--- bot/utils/gpt_summarizer.py
+++ bot/utils/gpt_summarizer.py
@@ -2,13 +2,14 @@ import openai
 import configparser
 import os 
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-config_file_path = os.path.join(current_dir, "config.ini")
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# config_file_path = os.path.join(current_dir, "config.ini")
 
 # Set API key
-config = configparser.ConfigParser()
-config.read(config_file_path)
-openai.api_key = config["OpenAI"]["api_key"]
+# config = configparser.ConfigParser()
+# config.read(config_file_path)
+# openai.api_key = config["OpenAI"]["api_key"]
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def get_summary(result):
     # Define prompt 
