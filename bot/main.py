e025e15ecf19db5d68836889148f42c2adb4346c
advait <advait001@e.ntu.edu.sg>
2023-10-22 00:35:05 +0800

    Remove call limits

--- bot/main.py
+++ bot/main.py
@@ -17,11 +17,6 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 PORT = int(os.environ.get("PORT", "5000"))
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
-CALL_COUNT = 0
-LAST_RESET_DATE = datetime.now().date()  # Track the last reset date
-LAST_MESSAGE_DATE = None  # This will store the date of the last processed message
-# Global variable to store the message counts per user
-USER_MESSAGE_COUNTS = {}
 
 # Enable logging
 logging.basicConfig(
@@ -44,29 +39,6 @@ async def help_command(update: Update, context):
 
 async def summarize_command(update: Update, context):
     """Summarize the conversation."""
-    global CALL_COUNT, LAST_RESET_DATE, USER_MESSAGE_COUNTS, LAST_MESSAGE_DATE
-    message_date = update.message.date.date()  # Get the date of the current message
-
-    # Check if this is the first message the bot is processing since it started
-    if LAST_MESSAGE_DATE is None:
-        LAST_MESSAGE_DATE = message_date
-
-    # If the date of the current message is different from the LAST_MESSAGE_DATE, it means a new day has started since the bot last processed a message
-    if LAST_MESSAGE_DATE != message_date:
-        logger.info("New day detected based on message date. Resetting counts.")
-        LAST_RESET_DATE = message_date
-        LAST_MESSAGE_DATE = message_date
-        USER_MESSAGE_COUNTS = {}
-        CALL_COUNT = 0
-
-    CALL_COUNT += 1
-
-    if CALL_COUNT > 20:
-        await update.message.reply_text(
-            "You have exceeded the maximum number of calls. Please try again later."
-        )
-        return
-
     logger.info("Summarizing conversation...")
     chat_id = update.effective_chat.id
 
@@ -87,23 +59,6 @@ async def summarize_command(update: Update, context):
             )
             return
 
-    # Get user ID and check if it's in the dictionary
-    user_id = update.effective_user.id
-
-    if user_id in USER_MESSAGE_COUNTS:
-        # If they're at or exceed the limit, don't continue
-        if USER_MESSAGE_COUNTS[user_id] + num_messages > 500:
-            remaining = 500 - USER_MESSAGE_COUNTS[user_id]
-            await context.bot.send_message(
-                chat_id=chat_id,
-                text=f"You only have {remaining} messages left to summarize.",
-            )
-            return
-        else:
-            USER_MESSAGE_COUNTS[user_id] += num_messages
-    else:
-        USER_MESSAGE_COUNTS[user_id] = num_messages
-
     try:
         result = await get_chat_history(chat_id, num_messages)
     except Exception as e:
@@ -118,7 +73,7 @@ async def summarize_command(update: Update, context):
     logger.info(f"Conversation summarized by {caller_info.name} ({caller_info.id})")
 
     prefix = f"_Conversation summarized by {caller_info.name} for the last {num_messages} messages:_\n\n"
-    postfix = f"\n\n({CALL_COUNT}/20)"
+    postfix = ""
 
     summary = prefix + get_summary(result) + postfix
     summary = summary.replace(".", "\.")
